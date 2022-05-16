46dbf76891ed66830c473ddeb8d4aaa70391131b
BharathRaoN <92871231+BharathRaoN@users.noreply.github.com>
2022-05-16 18:40:22 +0530

    Add files via upload

---
+++ BlackJack/script.js
@@ -0,0 +1,68 @@
+let player = {
+  name: "Per",
+  chips: 200,
+};
+
+let cards = [];
+let sum = 0;
+let alive = false;
+let blackjack = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+let playerEl = document.querySelector("#player-el");
+
+playerEl.textContent = `${player.name}  : $  ${player.chips}`;
+
+function isAlive() {
+  alive = false;
+  message = "you're out of the game 😭";
+}
+
+function isBlackJack() {
+  blackjack = true;
+  message = "whoaa!! you've got a blackjack 🥳";
+}
+function startGame() {
+  alive = true;
+  let firstCard = randomNumber();
+  let secondCard = randomNumber();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + "  ";
+  }
+  sumEl.textContent = "Sum:" + sum;
+
+  sum < 21
+    ? (message = "you're still in the game!!😃")
+    : sum === 21
+    ? isBlackJack()
+    : isAlive();
+
+  messageEl.innerHTML = message;
+}
+
+function newCard() {
+  if (alive === true && blackjack === false) {
+    let card = randomNumber();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
+
+function randomNumber() {
+  let random = Math.floor(Math.random() * 13) + 1;
+  if (random > 10) {
+    return 10;
+  } else if (random === 1) {
+    return 11;
+  } else return random;
+}
