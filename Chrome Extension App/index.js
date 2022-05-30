d2a6c98d30ba6744cd8011540619e884ab6ad2df
BharathRaoN <bharathraonoutlook.com>
2022-05-30 23:54:20 +0530

    learnt keypress event and other cool stuff, deployed my first extension in chrome will imrprove it later.

--- Chrome Extension App/index.js
+++ Chrome Extension App/index.js
@@ -1,7 +1,30 @@
+let myLead = [];
 
-inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click",()=>{
-    console.log('Button clicked through event listener');
-})
+inputEl.addEventListener("keypress", (Event) => {
+  if (Event.key == "Enter") {
+    Event.preventDefault();
+    inputBtn.click();
+  }
+});
 
+inputBtn.addEventListener("click", () => {
+  myLead.push(inputEl.value);
+  renderLeads();
+  inputEl.value = null;
+});
+
+function renderLeads() {
+  let listitems = "";
+  for (let i = 0; i < myLead.length; i++) {
+    listitems += `
+    
+    <li><a href= "${myLead[i]}" target="_blank"> ${myLead[i]}</a> </li>
+    
+    `;
+  }
+  ulEl.innerHTML = listitems;
+}
